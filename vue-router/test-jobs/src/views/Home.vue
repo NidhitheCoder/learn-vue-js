d6527b862f25dec01748dbeff99d93b4e584a360
NidhitheCoder <nidhpv@gmail.com>
2021-09-17 23:35:06 +0530

    refactor: Change contents inside the home page

--- vue-router/test-jobs/src/views/Home.vue
+++ vue-router/test-jobs/src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+      maiores aspernatur illo atque. Consequuntur provident assumenda, sint
+      doloribus error asperiores ea? Dignissimos sit porro impedit tempore ipsum
+      ab cumque voluptatum unde, accusantium debitis fuga soluta aliquam ipsa
+      consequatur laborum numquam ut repellat provident error! Expedita dolorem
+      dolores porro soluta quisquam nihil non ducimus pariatur libero, qui optio
+      at aliquid debitis ut reprehenderit ullam et voluptatibus, deserunt nisi
+      in error praesentium possimus sequi. Aliquid, neque! Quae dignissimos quo
+      saepe! Odio, nihil eius. Natus modi ex repellendus architecto, corporis
+      eveniet ipsam rerum nostrum sequi. Provident corporis necessitatibus vitae
+      harum est, quasi blanditiis!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea enim delectus
+      harum eos saepe, voluptatum quas aliquid eaque corrupti repellendus
+      minima, suscipit autem rem tenetur qui numquam optio? Laboriosam omnis
+      atque quas rem doloribus fugiat corrupti maxime odio nihil optio sit
+      deleniti impedit officiis eaque voluptatibus consequatur quos,
+      reprehenderit harum?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+      provident aliquam eligendi quisquam dolorem hic voluptatem repellendus
+      nobis architecto, dignissimos voluptates fugit esse dolorum! Beatae
+      aspernatur eveniet quibusdam, earum nisi molestiae in pariatur asperiores,
+      quaerat nihil error et iusto maiores deleniti, numquam praesentium quasi
+      omnis eos libero ipsa. Atque voluptatum suscipit numquam animi, blanditiis
+      cupiditate deserunt voluptatibus eveniet commodi quod libero culpa ullam
+      deleniti eum, illum totam iure optio facere ducimus magnam in vitae ad
+      exercitationem vero. Illo soluta magnam iure vel, debitis consequatur
+      porro quaerat cum. Sapiente quo maxime totam explicabo, itaque voluptas
+      nam in veritatis illo, ad nisi ullam eligendi eaque. Amet, officiis,
+      facilis optio dolorem natus vitae alias earum odit eaque sint perferendis
+      excepturi possimus. Dolor dolores nulla debitis molestias, incidunt
+      deserunt. Aut libero debitis sequi reprehenderit dicta ut unde rem, a
+      vitae quidem dignissimos aperiam esse similique non delectus porro
+      repellendus consequuntur eveniet. Facere, beatae. Mollitia voluptate
+      temporibus, minima distinctio nostrum totam deleniti fuga ad tenetur sunt,
+      voluptatem ullam consequuntur facilis enim accusantium veritatis nulla
+      natus excepturi. Quis corrupti hic vel ex, maxime excepturi sed
+      accusantium adipisci cumque debitis placeat nisi facere, unde
+      exercitationem ut quibusdam delectus enim aliquam esse deleniti ipsam.
+      Nihil accusamus corrupti enim.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
