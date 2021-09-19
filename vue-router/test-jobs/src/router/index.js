85bb3ea01fed2a8d53a739c65d52126062dd0471
NidhitheCoder <nidhpv@gmail.com>
2021-09-19 22:32:45 +0530

    add: redirects and 404 notfound pages

--- vue-router/test-jobs/src/router/index.js
+++ vue-router/test-jobs/src/router/index.js
@@ -3,6 +3,7 @@ import About from '../views/About.vue'
 import Home from '../views/Home.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import jobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -25,7 +26,18 @@ const routes = [
     name: 'JobDetails',
     component: jobDetails,
     props: true,
-  }
+  },
+  // Redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // Catch all 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+  },
 ]
 
 const router = createRouter({
