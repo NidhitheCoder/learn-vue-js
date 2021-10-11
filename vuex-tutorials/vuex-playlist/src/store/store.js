017c32e063a2df1753a2c7c271c1b110504a8885
NidhitheCoder <nidhpv@gmail.com>
2021-10-11 23:09:34 +0530

    Mutations in vuex store

--- vuex-tutorials/vuex-playlist/src/store/store.js
+++ vuex-tutorials/vuex-playlist/src/store/store.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+  strict: true,
   state: {
     products: [
       { name: "Banana skin", price: 20 },
@@ -21,6 +22,13 @@ export const store = new Vuex.Store({
         }
       });
       return salesProducts;
+    }
+  },
+  mutations: {
+    reducePrice : state => {
+      state.products.forEach( product => {
+        product.price -= 1;
+      })
     }
   }
 })
