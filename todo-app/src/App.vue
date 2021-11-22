2f7f6c108038c2ba810f68b05ea9c1267d9fc633
NidhitheCoder <nidhpv@gmail.com>
2021-11-22 23:54:51 +0530

    feat: todo list intitial app state

---
+++ todo-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <TodoList msg="Toto list component"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
