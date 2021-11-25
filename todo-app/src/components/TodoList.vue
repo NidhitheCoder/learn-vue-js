923cdd15ea6766996340ac7f6f3794b538ae18f4
NidhitheCoder <nidhpv@gmail.com>
2021-11-25 17:02:30 +0530

    feat: update status on click

--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -3,8 +3,15 @@
     <h2 class="text-center mt-5">{{ msg }}</h2>
     <!-- Input -->
     <div class="d-flex">
-      <input v-model="task" type="text" placeholder="Enter text" class="form-control">
-      <button @click="submitTask" class="btn btn-warning rounded-0">SUBMIT</button>
+      <input
+        v-model="task"
+        type="text"
+        placeholder="Enter text"
+        class="form-control"
+      />
+      <button @click="submitTask" class="btn btn-warning rounded-0">
+        SUBMIT
+      </button>
     </div>
     <!-- Task table -->
     <table class="table table-bordered mt-5">
@@ -18,58 +25,63 @@
       </thead>
       <tbody>
         <tr v-for="(task, index) in tasks" :key="index">
-          <td>{{task.name}}</td>
-          <td>{{task.status}}</td>
+          <td>{{ task.name }}</td>
+          <td style="width: 120px">
+            <span class="pointer" @click="changeStatus(index)">{{
+             firstCharUpper(task.status)
+            }}</span>
+          </td>
           <td>
             <div class="text-center" @click="editTask(index)">
-              <span class="fa fa-pen"></span>
+              <span class="fa fa-pen pointer"></span>
             </div>
           </td>
           <td>
             <div class="text-center" @click="deleteTask(index)">
-              <span class="fa fa-trash"></span>
+              <span class="fa fa-trash pointer"></span>
             </div>
           </td>
         </tr>
-      </tbody >
+      </tbody>
     </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TodoList',
+  name: "TodoList",
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      task: '',
+      task: "",
       editedTask: null,
-      tasks : [
+      availableStatuses: ["to-do", "inProgress", "finished"],
+      tasks: [
         {
-          name: 'Steal banana from the store',
-          status: 'to-do'
+          name: "Steal banana from the store",
+          status: "to-do",
         },
         {
-          name: 'Eat 1kg chocolate in 1 our',
-          status: 'in-progress'
-        }, 
+          name: "Eat 1kg chocolate in 1 our",
+          status: "in-progress",
+        },
         {
-          name: 'jumb over the road for 30 minutes',
-          status: 'in-progress'
-        }
-      ]
-    }
+          name: "jumb over the road for 30 minutes",
+          status: "in-progress",
+        },
+      ],
+    };
   },
   methods: {
-    submitTask(){
-      if(this.task.length === 0) return;
+    submitTask() {
+      if (this.task.length === 0) return;
 
-      if(this.editedTask === null) {
+      if (this.editedTask === null) {
         this.tasks.push({
           name: this.task,
-          status: 'to-do',
+          status: "to-do",
         });
       } else {
         this.tasks[this.editedTask].name = this.task;
@@ -81,13 +93,23 @@ export default {
       this.tasks.splice(taskIndex, 1);
     },
     editTask(index) {
-     this.task = this.tasks[index].name;
-     this.editedTask = index;
-    }
-  }
-}
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+    changeStatus(index) {
+      let newIndex = this.availableStatuses.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.availableStatuses[newIndex];
+    },
+    firstCharUpper(value) {
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+.pointer {
+  cursor: pointer;
+}
 </style>
