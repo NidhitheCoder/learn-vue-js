11967f5018b08510c6badb696ddd2675d59bf887
NidhitheCoder <nidhpv@gmail.com>
2021-11-26 16:59:20 +0530

    feat: data and methods

--- vue-netninja-course/vue-playlist/app.js
+++ vue-netninja-course/vue-playlist/app.js
@@ -2,6 +2,11 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Jao',
-
+    job: 'Doctor'
+  },
+  methods: {
+    greet: function(time) {
+      return 'Good ' + time + ' ' + this.name;
+    }
   }
 });
