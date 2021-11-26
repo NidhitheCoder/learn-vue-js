352383eaf170f2aa476706d42d40b73073aedb37
NidhitheCoder <nidhpv@gmail.com>
2021-11-26 17:52:35 +0530

    feat: events in vue js

---
+++ vue-netninja-course/vue-events/app.js
@@ -0,0 +1,23 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    age: 25,
+    x: 0,
+    y: 0,
+    color: 'red',
+  },
+  methods: {
+    add: function (inc){
+      this.age += inc;
+    },
+    substract: function(dec) {
+      this.age -= dec;
+    },
+    updateXY: function(event){
+      console.log(event)
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+      this.color ='rgb('+this.x+','+this.y+','+this.x+')';
+    }
+  }
+})
