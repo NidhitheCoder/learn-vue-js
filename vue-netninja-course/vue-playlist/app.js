7677d37f35b6e95bf4f8dfe4784bdc82cacb6be0
NidhitheCoder <nidhpv@gmail.com>
2021-11-26 17:21:14 +0530

    feat: data binding in vue js

--- vue-netninja-course/vue-playlist/app.js
+++ vue-netninja-course/vue-playlist/app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Jao',
-    job: 'Doctor'
+    job: 'Doctor',
+    website: 'https://www.thenetninja.co.uk',
+    redirectionTag: '<a href="https://www.thenetninja.co.uk">redirect</a>',
   },
   methods: {
     greet: function(time) {
