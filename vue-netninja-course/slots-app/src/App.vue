346783de3ee4c4bc9c8154af655df747b2e936ef
NidhitheCoder <nidhpv@gmail.com>
2021-12-15 23:17:46 +0530

    feat: dynamic form using slots in vue js

--- vue-netninja-course/slots-app/src/App.vue
+++ vue-netninja-course/slots-app/src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
-    <form-helper>
+    <!-- <form-helper>
       <h1 slot="title"> {{title}}</h1>
       <p slot="para">I am the paragraph for the slot</p>
+    </form-helper> -->
+    <form-helper>
+      <div slot="form-header">
+        <h3>This is the title of the form</h3>
+        <p>Information about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="Name">
+        <input type="password" placeholder="Password">
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
     </form-helper>
   </div>
 </template>
--- vue-netninja-course/slots-app/src/components/formHelper.vue
+++ vue-netninja-course/slots-app/src/components/formHelper.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="hello">
-    <slot name="title"></slot>
+    <!-- <slot name="title"></slot>
     <h1>I am the form helper</h1>
-    <slot name="para" ></slot>
+    <slot name="para" ></slot> -->
+    <h1>Please fill out form</h1>
+    <form action="">
+      <div id="form-header">
+        <slot name="form-header" />
+      </div>
+      <div id="form-fields">
+        <slot name="form-fields" />
+      </div>
+      <div id="form-control">
+        <slot name="form-control" />
+      </div>
+      <div id="useful-links">
+        <ul>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+          <li><a href="#">Link 4</a></li>
+        </ul>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -25,8 +45,33 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  color: red;
+form > div {
+  padding: 20px;
+  background: #eee;
+  margin: 20px 0;
 }
 
+h1 {
+  text-align: center;
+}
+
+#form-header {
+    background: #ddd;
+    border: 1px solid #bbb;
+}
+
+form {
+  width: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+#useful-links ul {
+  padding: 0;
+}
+
+#useful-links li {
+ display: inline-block;
+ margin-right: 10px;
+}
 </style>
