2f028b46154c59cc2ad3a1962f92d9621bf45dec
NidhitheCoder <nidhpv@gmail.com>
2021-12-21 23:47:11 +0530

    feat: add blog form component created

---
+++ vue-netninja-course/input-binding-app/src/components/addblog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="addblog">
+    <h2>Add a New Blog Post</h2>
+    <form>
+      <label>Blog Title:</label>
+      <input type="text" required>
+      <label>Blog Content:</label>
+      <textarea></textarea>
+    </form>
+    <div class="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog title:</p>
+      <p>Blog content:</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addBlog',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
