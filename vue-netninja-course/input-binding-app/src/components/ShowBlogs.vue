840ae60a99a5496c377aee6b72d58009bdec4409
NidhitheCoder <nidhpv@gmail.com>
2021-12-29 21:56:20 +0530

    feat: add get request using vue-resource

---
+++ vue-netninja-course/input-binding-app/src/components/ShowBlogs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div  v-for="blog in blogs" :key="blog.id" class="single-blog">
+      <h4>{{blog.title}}</h4>
+      <p>
+        {{blog.body}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      blogs: []
+    }
+  },
+  methods: {
+
+  },
+  created() {
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data) {
+      console.log(data)
+      this.blogs = data.body.slice(0,10)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
