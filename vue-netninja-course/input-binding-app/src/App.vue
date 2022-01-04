5446b1a79deee80667f25b083cf8bf86864eb26b
NidhitheCoder <nidhpv@gmail.com>
2022-01-04 19:03:20 +0530

    feat: listblogs component created{

--- vue-netninja-course/input-binding-app/src/App.vue
+++ vue-netninja-course/input-binding-app/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <add-blog></add-blog>
     <show-blogs></show-blogs>
+    <list-blogs></list-blogs>
   </div>
 </template>
 
 <script>
 import addblog from './components/addblog.vue'
 import showBlogs from './components/ShowBlogs.vue'
+import listBlogs from './components/ListBlogs.vue'
 
 export default {
   name: 'App',
   components: {
     'add-blog': addblog,
-    'show-blogs': showBlogs
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs,
   }
 }
 </script>
---
+++ vue-netninja-course/input-binding-app/src/components/ListBlogs.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="show-blogs">
+    <h1>List blog titles</h1>
+    <input type="text" v-model="search" placeholder="Search Blogs">
+    <div v-for="blog in filteredBlogs" :key="blog.id" class="single-blog">
+      <h4 v-rainbow>{{blog.title | to-uppercase}}</h4>
+      <p>
+        {{blog.body | snippet}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [],
+      search: ''
+    }
+  },
+  methods: {
+
+  },
+  created() {
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data) {
+      this.blogs = data.body.slice(0, 10);
+    })
+  },
+  computed: {
+    filteredBlogs: function() {
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      })
+    }
+  },
+  filters: {
+   
+  },
+  directives: {
+    
+  }
+}
+</script>
+
+<style scoped>
+#show-blogs {
+  max-width: 850px;
+  margin: 0 auto;
+  padding-bottom: 2rem;
+  background: lightsalmon;
+}
+
+.single-blog {
+  padding: 1rem;
+  margin: 1rem;
+  box-sizing:border-box;
+  border-radius: 8px;
+  background-color: lightblue;
+}
+</style>
+
