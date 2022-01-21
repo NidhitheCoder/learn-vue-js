fb7f97e3287c8987a79815acff401b67e065143e
NidhitheCoder <nidhpv@gmail.com>
2022-01-21 23:57:09 +0530

    feat: routing parameter intro

---
+++ vue-netninja-course/input-binding-app/src/components/singleBlog.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="single-blog">
+    <h1></h1>
+    <article></article>
+  </div>
+</template>
--- vue-netninja-course/input-binding-app/src/routes.js
+++ vue-netninja-course/input-binding-app/src/routes.js
@@ -1,9 +1,11 @@
 import showBlogs from './components/ShowBlogs';
 import addBlog from './components/Addblog';
 import listBlogs from './components/ListBlogs';
+import singleBlog from './components/singleBlog';
 
 export default [
   { path: '/', component: showBlogs },
   { path: '/add', component: addBlog },
-  { path: '/list', component: listBlogs }
+  { path: '/list', component: listBlogs },
+  { path: '/blog/:id', component: singleBlog }
 ]
