eab3e61033999640b6bc79605f078955115e7606
Nidheesh PV <nidheesh@vonnue.com>
2023-09-05 11:02:28 +0530

    create separate components for refs and reactive  and vmodels

--- vuejs-2.7.0/src/App.vue
+++ vuejs-2.7.0/src/App.vue
@@ -1,64 +1,14 @@
 <script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue';
-
-import LearnComputed from './components/LearnComputed.vue'
-
-const input = ref<HTMLInputElement>();
-const increment = ref(4);
-const diff = ref();
-
-const count = 0
-const colors = reactive({
-  count
-});
-const item = {
-  name: "hello"
-}
-const whole = reactive([
-  item, {
-    name: "hello2"
-  }, {
-    name: "hello3"
-  }, {
-    name: "hello4"
-  }
-])
-
-const state = reactive({ sample: 12 });
-
-// const { sample } = state;
-
-
-
-onMounted(() => {
-  input?.value?.focus();
-})
-
-const changes = (e: any) => {
-  increment.value = e.target.value;
-  console.log(whole.findIndex(element => (
-    element === item
-  )))
-
-  diff.value = whole.findIndex(element => (
-    element === reactive(item)
-  ))
-  // sample++ // this is not possible because of the destructured state will leads to connection lost
-}
-
+// import LearnComputed from './components/LearnComputed.vue';
+import VModelExamples from './components/VModelExamples.vue';
+// import RefsAndReactives from './components/RefsAndReactives.vue';
 </script>
 
 <template>
   <div class="wrapper">
-    <LearnComputed />
-    <p>helloo</p>
-    <input ref="input" @input="changes" />
-    <p>{{ increment }}</p>
-    <button @click="colors.count++">Click me</button>
-    <p>{{ colors.count }}</p>
-    <p>{{ whole }}</p>
-    <p>{{ item }}</p>
-    <p>{{ diff }}</p>
+    <!-- <LearnComputed /> -->
+    <!-- <RefsAndReactives /> -->
+    <VModelExamples />
   </div>
 </template>
 
---
+++ vuejs-2.7.0/src/components/RefsAndReactives.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { onMounted, reactive, ref } from 'vue';
+
+const input = ref<HTMLInputElement>();
+// const { sample } = state;
+
+onMounted(() => {
+    input?.value?.focus();
+})
+
+const increment = ref(4);
+const diff = ref();
+
+const count = 0
+const colors = reactive({
+    count
+});
+const item = {
+    name: "hello"
+}
+
+
+const whole = reactive([
+    item, {
+        name: "hello2"
+    }, {
+        name: "hello3"
+    }, {
+        name: "hello4"
+    }
+])
+
+const state = reactive({ sample: 12 });
+
+const changes = (e: any) => {
+    increment.value = e.target.value;
+    console.log(whole.findIndex(element => (
+        element === item
+    )))
+
+    diff.value = whole.findIndex(element => (
+        element === reactive(item)
+    ))
+    // sample++ // this is not possible because of the destructured state will leads to connection lost
+}
+
+</script>
+
+<template>
+    <h1>Refs and Reactive</h1>
+    <input ref="input" @input="changes" />
+    <p>{{ increment }}</p>
+    <button @click="colors.count++">Click me</button>
+    <p>{{ colors.count }}</p>
+    <p>{{ whole }}</p>
+    <p>{{ item }}</p>
+    <p>{{ diff }}</p>
+    <p>{{ state }}</p>
+</template>
+
+<style scoped></style>
