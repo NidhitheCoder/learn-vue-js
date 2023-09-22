ffe75d6509480ec5cd3fc30fbcbe2cbd1f4dff03
Nidheesh PV <nidheesh@vonnue.com>
2023-09-22 15:26:29 +0530

    update some more life cycle hooks

--- vuejs-2.7.0/src/App.vue
+++ vuejs-2.7.0/src/App.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
-// import ComponentIndex  from './components/ComponentVModel/ComponentIndex.vue'
+import { ref } from 'vue';
+import ComponentIndex from './components/ComponentVModel/ComponentIndex.vue';
 // import LearnComputed from './components/LearnComputed.vue';
 // import VModelExamples from './components/VModelExamples.vue';
 // import RefsAndReactives from './components/RefsAndReactives.vue';
-import LearnLifeCyclehooks from './components/LearnLifeCyclehooks.vue';
+import LearnLifeCycleHooks from './components/LearnLifeCycleHooks.vue';
+
+const shouldShowForm = ref(true);
+
+const onToggleClick = () => shouldShowForm.value = !shouldShowForm.value;
 </script>
 
 <template>
   <div class="wrapper">
+    <button @click="onToggleClick" class="toggle-button">Toggle</button>
     <!-- <LearnComputed /> -->
     <!-- <RefsAndReactives /> -->
     <!-- <VModelExamples /> -->
-    <!-- <ComponentIndex /> -->
-    <LearnLifeCyclehooks />
+    <ComponentIndex v-if="shouldShowForm" />
+    <LearnLifeCycleHooks v-if="!shouldShowForm" />
   </div>
 </template>
 
@@ -31,5 +37,14 @@ header {
   justify-content: center;
   align-items: center;
   flex-direction: column;
+}
+
+.toggle-button {
+  background-color: aquamarine;
+  color: rgb(94, 5, 94);
+  padding: 0.5rem 1rem;
+  border-radius: 8px;
+  outline: none;
+  border: none;
 }
 </style>
--- vuejs-2.7.0/src/components/LearnLifeCycleHooks.vue
+++ vuejs-2.7.0/src/components/LearnLifeCycleHooks.vue
@@ -1,14 +1,35 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-const el = ref(0);
+import { onBeforeMount, onMounted, onUpdated, onUnmounted, ref } from 'vue';
+
+const defaultNumber = 0;
+const incrementValue = 2;
+const number = ref(defaultNumber);
+
+onBeforeMount(() => {
+    console.log("Before mount")
+})
 
 onMounted(() => {
-    console.log(el.value)
+    console.log("Mounted", number.value)
 })
+
+onUpdated(() => {
+    console.log("Updated", number.value)
+})
+
+onUnmounted(() => {
+    console.log("Unmounted");
+})
+
+const onUpdateButtonClick = () => {
+    number.value += incrementValue;
+}
 </script>
 
 <template>
     <h2>Life Cycle hooks in Vue js</h2>
+    <p>{{ number }}</p>
+    <button @click="onUpdateButtonClick">Update</button>
 </template>
 
 <style></style>
