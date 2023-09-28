335b2924b0e2bcd1cedecf5455bfeadfb3146a3d
Nidheesh PV <nidheesh@vonnue.com>
2023-09-28 09:38:30 +0530

    usefetch composables

--- vuejs-2.7.0/src/components/composables/LearnComposables.vue
+++ vuejs-2.7.0/src/components/composables/LearnComposables.vue
@@ -1,16 +1,24 @@
 <script setup>
-import { ref } from 'vue'
-import { useMouse } from './useMouse'
+import { ref } from 'vue';
+import { useMouse } from './useMouse';
+import { useFetch } from './useFetch';
+
 const ballRef = ref();
 const { x, y } = useMouse();
+const { data, isLoading, error } = useFetch('https://jsonplaceholder.typicode.com/users');
 
 </script>
 <template>
-    <div class="composable-container">
+    <div v-if="isLoading">Loading</div>
+    <div v-if="error">Error</div>
+    <div v-if="data" class="composable-container">
         <h1>Composables</h1>
-        <p>x: {{ x }}</p>
-        <p>y: {{ y }}</p>
         <div ref="ballRef" class="ball" :style="{ left: `${x}px`, top: `${y}px`, backgroundColor: `#${y}${x}` }"></div>
+        <div class="user-container" v-for="item in data" :key="item.id">
+            <p>{{ item.id }}</p>
+            <p>{{ item.name }}</p>
+            <p>{{ item.phone }}</p>
+        </div>
     </div>
 </template>
 <style>
@@ -32,5 +40,16 @@ const { x, y } = useMouse();
     position: absolute;
     left: var(--x-value);
     top: var(--y-value)
+}
+
+.user-container {
+    width: 70%;
+    height: auto;
+    background-color: gray;
+    border-radius: 4px;
+    padding: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 </style>
---
+++ vuejs-2.7.0/src/components/composables/useFetch.ts
@@ -0,0 +1,20 @@
+import { ref } from 'vue'
+
+export const useFetch = (url: string) => {
+  const data = ref(null)
+  const error = ref(null)
+  const isLoading = ref(true)
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      data.value = json
+      isLoading.value = false
+    })
+    .catch((err) => {
+      error.value = err
+      isLoading.value = false
+    })
+
+  return { data, isLoading, error }
+}
